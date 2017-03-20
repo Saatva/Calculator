94d0be0e2ddde860fdb8adac72ab3fa9a7500040
Todd Roper <toddroper@me.com>
2017-03-20 18:31:19 -0500

    Added basic JS file.

---
+++ calculator.js
@@ -0,0 +1,24 @@
+/**
+ * @description Calculator prototype.
+ * @constructor Calculator
+ */
+function Calculator()
+{
+    /**
+     * @description Assign this to variable for scope scenarios.
+     * @type {Calculator}
+     */
+    var self = this;
+
+
+    /**
+     * @description Init function builds the calculator.
+     */
+    this.init = function() {
+        /**
+         * This will initiate the object.
+         */
+        alert('Calculator initializing.');
+    };
+
+}
